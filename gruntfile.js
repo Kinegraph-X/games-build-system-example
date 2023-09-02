7dd42f1c558d00a08d4a268fcf81432988816d1e
sylvain <vassilyB@gmail.com>
2023-09-02 18:51:20 +0200

    Added package.json, gruntfile.js & app package sample (ShootEmUp.json)

---
+++ gruntfile.js
@@ -0,0 +1,52 @@
+var fs = require('fs');
+
+module.exports = function(grunt) {
+
+	var rootPath = 'codebase/', basePath, currentBundle;
+	currentBundle = grunt.cli.options.bundle;
+	basePath = rootPath + 'bundles/';
+	var bundleConfig = grunt.file.readJSON(currentBundle + '.json');
+	
+	var path = require('path');
+	var folderArray = bundleConfig.content;
+	
+	var configPath = [], browserifyPath = [];
+	folderArray.forEach(function(val, key) {
+		configPath.push(path.join(process.cwd(), rootPath + val));
+		browserifyPath.push(rootPath + val);
+	});
+	
+	var pkg = grunt.file.readJSON('package.json');
+	pkg.main = path.join(process.cwd(), basePath + currentBundle + '/index.js');
+	console.log(fs.existsSync(pkg.main));
+	// console.log(pkg);
+
+  // Project configuration.
+	require('load-grunt-config')(grunt, {
+		// path to config.js & task.js files, defaults to grunt dir
+		configPath: configPath,
+		overridePath: path.join(process.cwd(), basePath + currentBundle + '/grunt-config-' + currentBundle),
+		init : true,
+		data : {
+			rootPath : rootPath,
+			basePath : basePath,
+			currentProject : currentBundle,
+			pathToProject : basePath + currentBundle + '/',
+			browserifyPath : browserifyPath,
+			UIpackage : bundleConfig.UIpackage || ['minimal'],
+			ressourcePath : bundleConfig.ressourcePath
+		},
+		package : pkg
+		// postProcess : function (config) {
+			// config.package = pkg;
+			// return config;
+		// }
+	});
+	
+  // Default task(s).
+  grunt.registerTask('default', ['execute:debug', 'browserify:debug', 'exorcise:debug']);
+
+};
+
+
+
